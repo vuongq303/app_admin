3f66eb98f11dcf24688d8a30ea7e8ca325774871
vuongq303 <vuongq303@gmail.com>
2025-02-08 22:21:09 +0700

    a

--- lib/services/download_image.dart
+++ lib/services/download_image.dart
@@ -1,25 +1,25 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:image_downloader/image_downloader.dart';
+// import 'package:flutter_riverpod/flutter_riverpod.dart';
+// import 'package:image_downloader/image_downloader.dart';
 
-Future<String> downloadAllImages(List<String> imageUrls) async {
-  List<String> errors = [];
+// Future<String> downloadAllImages(List<String> imageUrls) async {
+//   List<String> errors = [];
 
-  for (String url in imageUrls) {
-    try {
-      var result = await ImageDownloader.downloadImage(url);
+//   for (String url in imageUrls) {
+//     try {
+//       var result = await ImageDownloader.downloadImage(url);
 
-      print('✅ Đã tải: $result');
-    } catch (e) {
-      errors.add('⚠️ Lỗi ngoại lệ: $url - $e');
-    }
-  }
+//       print('✅ Đã tải: $result');
+//     } catch (e) {
+//       errors.add('⚠️ Lỗi ngoại lệ: $url - $e');
+//     }
+//   }
 
-  return errors.isEmpty
-      ? 'Tải ảnh thành công'
-      : 'Có lỗi khi tải ảnh:\n${errors.join("\n")}';
-}
+//   return errors.isEmpty
+//       ? 'Tải ảnh thành công'
+//       : 'Có lỗi khi tải ảnh:\n${errors.join("\n")}';
+// }
 
-final downloadImageProvider =
-    FutureProvider.autoDispose.family<String, List<String>>((ref, listImage) {
-  return downloadAllImages(listImage);
-});
+// final downloadImageProvider =
+//     FutureProvider.autoDispose.family<String, List<String>>((ref, listImage) {
+//   return downloadAllImages(listImage);
+// });
